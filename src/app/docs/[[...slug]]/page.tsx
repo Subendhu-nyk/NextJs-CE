9a162d0abfbb6e186fdffe47fad467e971b1204d
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-13 20:07:16 +0530

    routes-(dynamic routes-(catch-all-segment) with default page)

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -5,10 +5,10 @@ const Docs = ({
     slug: string[];
   };
 }) => {
-  if (params.slug.length === 2) {
+  if (params.slug?.length === 2) {
     return <h1>View Docs for feature {params.slug[0]} and concept {params.slug[1]}</h1>;
   }
-  else if(params.slug.length===1){
+  else if(params.slug?.length===1){
     return <h1>View Docs for feature {params.slug[0]}</h1>;
   }
   return <h1>Docs Homepage</h1>;
