7aece5a1e9b9a93e3e6e8b582d377932552023fb
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-13 20:16:12 +0530

    custom not-found and programmatic not-found

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,9 @@
 // param props provided by next js to access the productId
+import { notFound } from "next/navigation";
 const ReviewDetail = ({params}:{params:{productId:string; reviewId:string}}) => {
+    if(parseInt(params.reviewId)>1000){
+        notFound();
+    }
     return (      
       <h1>Review {params.reviewId} for product {params.productId}</h1>       
     )
