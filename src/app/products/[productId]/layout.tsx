3cfd9e60a9dd679da24292a9dec165bc9edcb5dc
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-15 19:08:47 +0530

    Nested Layout for particular section

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,13 @@
+const ProductDetailsLayout=({
+    children,
+}:{children: React.ReactNode;})=>
+{
+    return(
+        <>
+        {children}
+        <h2>Features Product</h2>
+        </>
+    )
+}
+
+export default ProductDetailsLayout;
