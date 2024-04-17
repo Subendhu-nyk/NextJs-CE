227ccb42d286014c15ea7ff6ac1b24da637b6ee7
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-17 09:07:53 +0530

    Routing metadata and dynamic metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,12 @@
+// As description is not present then it will take the meta description from deepest segment i.e from app/layout.tsx 
+
+export const metadata = {
+  title: 'About us',  
+}
+
 const About = () => {
   return (
-    <h1>About us</h1>
+    <h1 className="text-4xl">About us</h1>
   )
 }
 
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,5 +1,22 @@
+import { Metadata } from "next"
+
+type Props ={
+  params:{
+    productId:string;
+  }
+}
+export const generateMetadata =({params}:Props): Metadata =>{
+  return{
+    title: `Product ${params.productId}`
+  }
+}
+
+
+
+
+
 // param props provided by next js to access the productId
-const ProductDetails = ({params}:{params:{productId:string}}) => {
+const ProductDetails = ({params}:Props) => {
     return (      
       <h1>Product Details {params.productId}</h1>       
     )
