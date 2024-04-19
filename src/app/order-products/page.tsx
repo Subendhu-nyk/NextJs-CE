41f3debdc80e521050e1affa83ceb4c1e7a3eb52
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-19 11:26:05 +0530

    navigating programatically update

--- src/app/order-products/page.tsx
+++ src/app/order-products/page.tsx
@@ -6,6 +6,8 @@ const OrderProduct = () => {
     const handleClick =()=>{
         console.log("placing Order")
         router.push('/')
+        // router.back() -it will navigate back to the previous page in the browser's history stack
+        //route.forward()- it will navigate to the next page 
     }
   return (
     <>
