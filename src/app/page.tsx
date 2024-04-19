416d32f035b37bbe3e943c743af7a590a6a2c0c8
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-19 09:12:49 +0530

    Navigation with Link component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import Link from "next/link"
+
 const Home = () => {
   return (
-    <h1>Hello world</h1>
+    <>  <h1>Hello world</h1>
+    <Link href='/blog'>Blog</Link>
+    <Link href='/products'>Products</Link>
+    </>
+  
+  
   )
 }
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link"
+
 const ProductList = () => {
+  const productId=100
   return (
     <>
+    <Link href='/'>Home</Link>
     <h1>ProductList</h1>
-    <h2>Product 1</h2>
-    <h2>Product 2</h2>
-    <h2>Product 3</h2>
+    <h2><Link href='/products/1'>Product 1</Link></h2>
+    <h2><Link href='/products/2'>Product 2</Link></h2>    
+    <h2><Link href='/products/3' replace>Product 3</Link></h2>
+    <h2><Link href={`/products/${productId}`}>Product {productId}</Link></h2>
     </>
+    // replace:replace prop replaces the current history state instead of adding a new URL to this stag this some thing 
   )
 }
 
