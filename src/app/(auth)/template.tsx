47ad099c720b29c2fbd33a71711050c95ddcc826
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-20 17:05:12 +0530

    templates

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import './style.css'
+import { useState } from "react";
+
 
 
 // usePathname only works in client Component, add the useClient directive at the top of the file to use it.All react components are server component by default and hooks can only be used inside client components.
@@ -16,10 +18,14 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-    const pathname=usePathname()   
+    const pathname=usePathname()  
+    const [input,setInput]=useState("")
 
   return (
-    <div>      
+    <div> 
+      <div>
+      <input value={input} onChange={e=>setInput(e.target.value)}/>  
+      </div>     
       {navLinks.map((link) => { 
          const isActive=pathname.startsWith(link.href)
          return (
