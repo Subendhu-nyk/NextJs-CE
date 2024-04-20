7fed3cb987d6ea01959d85a8c6cede0fe4ee7f6d
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-20 19:03:54 +0530

    Error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,18 @@
 // param props provided by next js to access the productId
 import { notFound } from "next/navigation";
+
+const getRandomInt=(count:number)=>{
+return Math.floor(Math.random()*count)
+}
+
+
 const ReviewDetail = ({params}:{params:{productId:string; reviewId:string}}) => {
+
+  const random=getRandomInt(2)
+  if(random===1){
+    throw new Error("Error in loading review")
+  }
+
     if(parseInt(params.reviewId)>1000){
         notFound();
     }
