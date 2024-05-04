929c2cb1a7a2bb2212157364c277697e1808b2d4
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-04 20:14:16 +0530

    intercepting routing conventions

---
+++ src/app/f1/f4/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const F4 = () => {
+    return (
+        <>
+        <div>F4 page</div>
+        <div>
+          <Link href="/f1/f3">F3</Link>
+          <Link href="/about">About</Link>
+        </div>  
+        </>
+          
+        )
+}
+
+export default F4
